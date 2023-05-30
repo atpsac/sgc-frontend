982af7e3fe3c1c017a86bc14f94fc0d5707289e0
Cristian Angulo <paul.angulo.anyosa@gmail.com>
2023-05-30 17:51:45 -0500

    SGC - Initial Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NotpagefoundComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,27 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: PagesComponent,
+        children: [
+            { path: '', component: DashboardComponent },
+            //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+        ]
+    },
+
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class PagesRoutingModule { }
